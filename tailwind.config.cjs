3d4aef05422a453c86205f590efdb9e504e69286
0xzay <0xzayeth@gmail.com>
2023-06-12 22:51:42 +0500

    fix: fix logo animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,12 +32,12 @@ module.exports = {
     extend: {
       keyframes: {
         enter: {
-          "0%": { opacity: 0, transform: "scale(0.95)" },
+          "0%": { opacity: 0, transform: "scale(0.99)" },
           "100%": { opacity: 1, transform: "scale(1)" },
         },
       },
       animation: {
-        enter: "enter 3s ease",
+        enter: "enter 1s ease",
       },
     },
   },
